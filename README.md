60d48e5046eb67a3ae483915eba4ed2ecadb5aad
caprice_dev <108645246+caprice1026-disc@users.noreply.github.com>
2023-05-13 03:44:26 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# voice4
+## Now debugging!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+これは、Javascriptのvoicerecognitionを使用して自身の発した音声を文字起こしし、それをchatGPTに投げて返信をVOICEBOXのずんだもんに喋らせるアプリケーションです。が、完成していないので注意が必要。
+
+## VOICEBOXについて
+
+VOICEBOXはDockerイメージが存在しているのでローカル以外で動かす場合はそちらを入れて使うといいですよ。
+
+## ToDo
+
+The audio output from the API is a wav file, not an MP3. I'll fix that somehow.
+
---
+++ main.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template , request, Response
+import requests
+import io 
+import uuid
+import os
+from io import BytesIO
+import openai
+import json
+from werkzeug.utils import secure_filename
+import pyaudio
+import wave
+import speech_recognition as sr
+from asyncio import sleep
+import asyncio
+import shutil
+
+#余計なライブラリは後で削ること
+#環境変数は後で書きなおすこと
+openai.api_key = ""
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/process_text", methods=["POST"])
+def process_text():
+    data = request.form["text"]
+    transcript = data['text']
+ #会話の内容についての制御部分
+    SYSTEM_MESSAGE = [
+        #日本語でやり取りするならここは日本語で指定した方が正確になるイメージ。特に敬語周りとか
+        {'role': 'system', 'content': '敬語を使うのをやめてください。次のように行動してください。語尾になのだをつけてください。あなたは、ずんだもんというずんだもちの妖精です。陽気で明るくて、少し変なところがありますがとてもかわいらしい子です。'},
+    ]
+ #ここで会話の内容を取得する
+    exit_flag = False
+    while not exit_flag:
+     SYSTEM_PROMPTS = SYSTEM_MESSAGE + [{'role': 'user', 'content': transcript}]
+    response = openai.Completion.create(
+       engine="gpt3.5-turbo",
+         messages = SYSTEM_PROMPTS,
+         temperature=0.9,
+     max_tokens=1500,
+    )
+    #ここで会話の内容を取得する
+    generated_text = response.choices[0].text.strip()
+    return generated_text
+
+#この内容をtextとしてpost_audio_queryに渡す
+
+def post_audio_query(generated_text: str) -> dict:
+    params = {'text': generated_text , 'speaker': 1}
+    res = requests.post('http://localhost:50021/audio_query', params=params)
+    return res.json()
+
+def post_synthesis(audio_query_response: dict) -> bytes:
+   params = {'speaker': 1}
+   headers = {'content-type': 'application/json'}
+   audio_query_response_json = json.dumps(audio_query_response)
+   res = requests.post(
+        'http://localhost:50021/synthesis',
+        data=audio_query_response_json,
+        params=params,
+        headers=headers
+    )
+   return Response(res.content, mimetype='audio/wav')
+
+   
+if __name__ == "__main__":
+    app.run(debug=True)
